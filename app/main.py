b0e1d3312c9077830687318728605c4d14a5d88f
bhwk <bryanhawaikit@gmail.com>
2025-07-12 17:25:12 +0800

    feat: added missing test route

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return status.HTTP_200_OK
+
+
+@app.get("/test", status_code=status.HTTP_200_OK)
+def test_route():
+    return {"message": "route is active"}
