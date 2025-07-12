4874a4def83839850207a226fab9566dceb0ed8c
bhwk <bryanhawaikit@gmail.com>
2025-07-12 17:23:05 +0800

    dev: add a missing route. this should fail.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,9 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_route():
+    response = client.get("/test")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "route is active"}
